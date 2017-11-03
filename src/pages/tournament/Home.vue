329d31e4a813ea87dd368d4dd4e6293eee253925
nswa17 <nswa17@gmail.com>
2017-11-03 16:39:18 +0900

    Add info-card to tournament top

--- src/pages/tournament/Home.vue
+++ src/pages/tournament/Home.vue
@@ -2,6 +2,14 @@
   .router-view-content(v-if="target_tournament")
     section.page-header
       h1 {{ target_tournament.name }}
+    el-card(v-if="compiled_markdown !== ''").info-card.admin
+      div.info-card-header(slot="header")
+        h5(style="opacity: 0") {{ '----' }}
+        .title
+          h3 Important Notice
+        .time
+          h5 {{ info_time }}
+      div.info-card-body(v-html="compiled_markdown")
     section
       link-list
         router-link(to="audience")
@@ -16,6 +24,7 @@
 import { mapState, mapGetters, mapActions } from 'vuex'
 import link_list from 'components/link-list.vue'
 import link_list_item from 'components/link-list-item.vue'
+import marked from 'marked'
 
 export default {
   components: {
@@ -29,7 +38,20 @@ export default {
     ]),
     ...mapGetters([
       'target_tournament'
-    ])
+    ]),
+    compiled_markdown () {
+      let info = this.target_tournament.user_defined_data.info
+      return marked(info ? info.text : '', { sanitize: true })
+    },
+    info_time () {
+      let time = this.target_tournament.user_defined_data.info.time
+      if (time !== undefined) {
+        let date = new Date(time)
+        return date.toLocaleTimeString('en-US', { hour12: false, hour: '2-digit', minute:'2-digit' })
+      } else {
+        return ''
+      }
+    }
   }
 }
 </script>
