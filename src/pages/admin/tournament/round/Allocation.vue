0c0b6bc4403b6fd1157e6e95829ce75dcca206a5
nswa17 <nswa17@gmail.com>
2017-11-16 13:17:54 +0900

    Bugfix

--- src/pages/admin/tournament/round/Allocation.vue
+++ src/pages/admin/tournament/round/Allocation.vue
@@ -141,7 +141,7 @@
                   el-option(v-for="algorithm in ['standard', 'traditional']", :key="algorithm", :value="algorithm", :label="algorithm")
               el-form-item(v-for="sub_label in ['chairs', 'panels', 'trainees']", :key="sub_label", :label="capitalize(sub_label)+' per venue'", v-if="label === 'all' || label === 'adjudicators'")
                 el-input-number(v-model="dialog.draw.form.model.numbers_of_adjudicators[sub_label]", :min="{ chairs: 1, panels: 0, trainees: 0 }[sub_label]")
-              el-form-item(label="Considering Rounds", v-if="!dialog.draw.form.model.shuffle")
+              el-form-item(label="Considering Rounds", v-if="['all', 'teams', 'adjudicators'].includes(label) || !dialog.draw.form.model.shuffle")
                 el-checkbox(v-model="dialog.draw.consider_all") All Rounds Before {{ target_round.name }}
                 el-select(v-if="!dialog.draw.consider_all", v-model="dialog.draw.considering_rs", multiple, :disabled="label === 'venues' && dialog.draw.form.model.shuffle")
                   el-option(v-for="round in target_tournament.rounds.slice().sort((r1, r2) => r1.r > r2.r ? 1 : -1)", :key="round.r", :value="round.r", :label="round.name")
